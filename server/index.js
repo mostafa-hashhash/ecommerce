d24c27b1184dee16fe3c7c9ff88275030eebfd01
Alshafei2430 <alshafei.2430@gmail.com>
2020-09-16 14:35:14 +0200

    initializing the server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(morgan('tiny'))
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
